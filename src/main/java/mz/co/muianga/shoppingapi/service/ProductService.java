f377a0727e2ea6194a580695c2b200b2ae30b21e
Nilvandro Muianga <nilvandro.muianga@vm.co.mz>
2025-02-08 19:43:42 +0200

    chore: replace hardcoded values of hosts for env vars

--- src/main/java/mz/co/muianga/shoppingapi/service/ProductService.java
+++ src/main/java/mz/co/muianga/shoppingapi/service/ProductService.java
@@ -1,24 +1,22 @@
 package mz.co.muianga.shoppingapi.service;
 
 import mz.co.muianga.shoppingclient.dto.ProductDTO;
-import mz.co.muianga.shoppingclient.exception.ProductNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ProductService {
 
-    public ProductDTO getProductByIdentifier(String identifier) {
-        try {
-            RestTemplate restTemplate = new RestTemplate();
-            var url = "http://localhost:8081/products/" + identifier;
-            ResponseEntity<ProductDTO> response = restTemplate.getForEntity(url, ProductDTO.class);
+    @Value("${PRODUCT_API_URL:http://localhost:8081/products/}")
+    private String productApiUrl;
 
-            return response.getBody();
-        } catch(HttpClientErrorException.NotFound e) {
-            throw new ProductNotFoundException();
-        }
+    public ProductDTO getProductByIdentifier(String identifier) {
+        RestTemplate restTemplate = new RestTemplate();
+        var url = productApiUrl + identifier;
+        ResponseEntity<ProductDTO> response = restTemplate.getForEntity(url, ProductDTO.class);
+
+        return response.getBody();
     }
 }
--- src/main/java/mz/co/muianga/shoppingapi/service/UserService.java
+++ src/main/java/mz/co/muianga/shoppingapi/service/UserService.java
@@ -1,32 +1,32 @@
 package mz.co.muianga.shoppingapi.service;
 
 import mz.co.muianga.shoppingclient.dto.UserDTO;
-import mz.co.muianga.shoppingclient.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-
 @Service
 public class UserService {
 
+    @Value("${USER_API_URL:http://localhost:8080/users/}")
+    private String userApiUrl;
+
     public UserDTO getUserByCpf(String cpf, String key) {
-        try {
-            RestTemplate restTemplate = new RestTemplate();
-            String url = "http://localhost:8080/users/" + cpf;
+        RestTemplate restTemplate = new RestTemplate();
+        String url = userApiUrl + cpf;
 
-            UriComponents builder = UriComponentsBuilder.newInstance()
-                    .scheme("http").host("localhost:8080").path("/users/" + cpf)
-                    .queryParam("key", key).build();
+        UriComponents builder = UriComponentsBuilder.fromUriString(url)
+                .queryParam("key", key)
+                .build();
 
-            ResponseEntity<UserDTO> response = restTemplate.getForEntity(builder.toString(), UserDTO.class);
-            return response.getBody();
-        }catch (HttpClientErrorException.NotFound e) {
-            throw new UserNotFoundException();
-        }
+        /*UriComponents builder = UriComponentsBuilder.newInstance()
+                .scheme("http").host("localhost:8080").path("/users/" + cpf)
+                .queryParam("key", key).build();*/
+
+        ResponseEntity<UserDTO> response = restTemplate.getForEntity(builder.toString(), UserDTO.class);
+        return response.getBody();
     }
 }
